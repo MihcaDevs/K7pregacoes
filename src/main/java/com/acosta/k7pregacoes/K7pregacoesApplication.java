d94a3769a97fa74e9ce979c123f072cd6eeec653
Alberto Costa <mihcadevs@gmail.com>
2019-08-23 11:55:30 -0300

    Criação do projeto

---
+++ src/main/java/com/acosta/k7pregacoes/K7pregacoesApplication.java
@@ -0,0 +1,13 @@
+package com.acosta.k7pregacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K7pregacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K7pregacoesApplication.class, args);
+	}
+
+}
