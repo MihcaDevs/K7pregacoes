b648a00aecbd3507c556f02297a0b69bf613e243
Alberto Costa <mihcadevs@gmail.com>
2019-08-23 13:49:51 -0300

    Mapeamento da classe Pregador e criação automatica da base de dados no banco H2

--- src/main/java/com/acosta/k7pregacoes/domain/Pregador.java
+++ src/main/java/com/acosta/k7pregacoes/domain/Pregador.java
@@ -2,11 +2,20 @@ package com.acosta.k7pregacoes.domain;
 
 import java.io.Serializable;
 
-public class Pregador implements Serializable {
-	
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pregador implements Serializable {	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
 	
 	public Pregador() {	
 		
